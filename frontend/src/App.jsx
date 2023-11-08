8a9defe6cf894eeff3b3d24848c86550dbd3ac63
hipticem <maamir.hipticem@gmail.com>
2023-11-08 10:24:17 +0100

    recuperation des items et affichage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,15 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
+import { useLoaderData } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
+  const items = useLoaderData();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {items.map((item) => (
+        <h2 key={item.id}>{item.title}</h2>
+      ))}
     </div>
   );
 }
